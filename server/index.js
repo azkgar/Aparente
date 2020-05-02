55bd1d4ec32532f540ab5961efe74c49d95e2112
Azkary Garcia <macbookpro@azkarys-mbp.lan>
2020-05-02 17:50:00 -0500

    Adds backend folder and npm packages

---
+++ server/index.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+const mongoose = require ("mongoose");
+const passport = require ("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const findOrCreate = require ("mongoose-findorcreate");
