55de29e3ca2b3365266fcf45763bb388d54761b3
Azkary Garcia <macbookpro@azkarys-mbp.lan>
2020-05-04 11:40:00 -0500

    Create server.js file

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = porcess.env.PORT||5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true});
+const connection = mongoose.connection;
+connection.once("open",()=>{
+    console.log("MongoDB sí está jalando chido");
+});
+
+const postRouter = require("./routes/post");
+const writersRouter = require("./routes/writers");
+
+app.listen(port,()=>{
+    console.log("Servidor corriendo en el 5000");
+});
