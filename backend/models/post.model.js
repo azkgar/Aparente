f3d14aa4f75b60a3fd88b829d3d128cb0d9e600c
Azkary Garcia <macbookpro@azkarys-mbp.lan>
2020-05-04 13:01:31 -0500

    Backend finished. Adds no duplicate title in the post schema

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema;
 const postSchema = new Schema ({
     title: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     date: {
         type:Date,
