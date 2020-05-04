cb53fa4b85bfd6ef68703f9a0baf19d8c15e5f50
Azkary Garcia <macbookpro@azkarys-mbp.lan>
2020-05-04 16:43:43 -0500

    Create a new component for adding users to the writing Schema

---
+++ src/components/Create-User.component.jsx
@@ -0,0 +1,77 @@
+import React , {Component} from "react";
+import axios from "axios";
+
+export default class CreateUser extends Component{
+    constructor(props){
+        super (props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            username: "",
+            password: ""
+        }
+    }
+
+    onChangeUsername(e){
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onChangePassword(e){
+        this.setState({
+            password: e.target.value
+        });
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+        const user = {
+            username: this.state.username,
+            password: this.state.password
+        }
+
+        console.log(user);
+
+        axios.post("http://localhost:5000/writescribir/writescritor/nuevo", user)
+            .then(res => console.log(res.data));
+        
+        this.setState({
+            username: "",
+            password: ""
+        });
+    }
+    render(){
+        return(
+            <div>
+                <h2>Registra un nuevo escritor</h2>
+                <form onSubmit = {this.onSubmit}>
+                    <div className = "form-group">
+                        <label>Nombre de usuario: </label>
+                        <input type = "text"
+                        required
+                        className = "form-control"
+                        value = {this.state.username}
+                        onChange = {this.onChangeUsername}
+                        />
+                    </div>
+                    <div className = "form-group">
+                        <label>Contraseña: </label>
+                        <input type = "password"
+                        required
+                        className = "form-control"
+                        value = {this.state.password}
+                        onChange = {this.onChangePassword}
+                        />
+                    </div>
+                    <div className = "form-group">
+                        <input type = "submit" value = "Crear nuevo escritor" className = "btn btn-primary"/>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
