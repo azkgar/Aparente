1110b91783bf2cd40b5f626c7bfcadfb4e519655
Azkary Garcia <macbookpro@azkarys-mbp.lan>
2020-05-04 13:15:43 -0500

    Adds packages axios, react router dom & date picker for front end development

---
+++ src/components/Create-Post.component.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+export default class CreatePosts extends Component{
+    constructor(props){
+        super(props);
+    }
+}
